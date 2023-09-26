b8ae98b420a4761dbce545bd9bea0fefe9234a86
John Hortua <1109660285@u.icesi.edu.co>
2023-09-26 15:53:53 -0500

    Implemented the Iterable interface but methods are unimplemented

--- src/structures/HashTable.java
+++ src/structures/HashTable.java
@@ -1,10 +1,29 @@
 package src.structures;
-import java.util.HashMap;
-public class HashTable<K, V> {
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.function.Consumer;
+
+public class HashTable<K, V> implements Iterable<V> {
+
+    @SuppressWarnings("unchecked")
+    private final LinkedList<Entry<K, V>>[] table = new LinkedList[16];
+
+    public HashTable() {
+    }
 
     public void add(V item) {}
     public void add(K key, V item) {}
     public void remove(V item) {}
     public V get(K key) { return null; }
 
+    @Override
+    public Iterator<V> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super V> action) {
+        Iterable.super.forEach(action);
+    }
 }
