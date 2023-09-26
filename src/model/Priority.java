dc197622efa91bf01f499b60644f32d5b6e5d36a
John Hortua <1109660285@u.icesi.edu.co>
2023-09-26 10:52:27 -0500

    Added Priority enum

---
+++ src/model/Priority.java
@@ -0,0 +1,14 @@
+package src.model;
+
+public enum Priority {
+
+    PRIORITARY(0), NON_PRIORITARY(1);
+
+    private final int priority;
+
+    private Priority(int priority) {
+        this.priority = priority;
+    }
+
+
+}
