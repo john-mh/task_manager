3295d0b463a0296f8c7161ab3a1845ecca51f73f
John Hortua <1109660285@u.icesi.edu.co>
2023-10-03 20:26:44 -0500

    Added documentation.

--- src/structures/PriorityQueue.java
+++ src/structures/PriorityQueue.java
@@ -8,22 +8,47 @@ public class PriorityQueue<T extends Comparable<T>> {
         heap = new Heap<>();
     }
 
+    /**
+     * Returns the element at the top of the heap without removing it.
+     *
+     * @return The element at the top of the heap
+     */
     public T peek() {
         return heap.peek();
     }
 
+    /**
+     * Returns the number of elements currently in the heap.
+     *
+     * @return The heap size
+     */
     public int size() {
         return heap.size();
     }
 
+    /**
+     * Returns whether the heap is empty.
+     *
+     * @return True if the heap is empty, false otherwise
+     */
     public boolean isEmpty() {
         return heap.isEmpty();
     }
 
+    /**
+     * Inserts an item into the priority queue.
+     *
+     * @param item The item to insert.
+     */
     public void add(T item) {
         heap.add(item);
     }
 
+    /**
+     * Retrieves and removes the head of this queue, or returns null if this queue is empty.
+     *
+     * @return The head of the queue, or null if the queue is empty.
+     */
     public T poll() {
         return heap.poll();
     }
