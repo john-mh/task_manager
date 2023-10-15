efdc481492c68967af183a820c4d573b9e34dd45
John Hortua <1109660285@u.icesi.edu.co>
2023-10-14 20:36:31 -0500

    Changed class to record

--- src/structures/Entry.java
+++ src/structures/Entry.java
@@ -1,19 +1,4 @@
-package structures;
+package src.structures;
 
-public class Entry<K, V> {
-    private final K key;
-    private final V value;
-
-    public Entry(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() {
-        return this.key;
-    }
-
-    public V getValue() {
-        return this.value;
-    }
+public record Entry<K, V>(K key, V value) {
 }
